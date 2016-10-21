b9247983aa26c8483d5c775d67c2651400c073f8
cholanmadala <cholanmadala1510@gmail.com>
2016-10-21 18:14:04 +0530

    Changed the variables and added comments and some structure changes

--- DataStructuresAndAlgorithms/js/main.js
+++ DataStructuresAndAlgorithms/js/main.js
@@ -9,16 +9,22 @@ function dataPrep(x) {
 }
 
 function insertionSort() {
-	var x = dataPrep(document.getElementById("dataInput").value);
-	for (var i = 1; i < x.length; i++) {
-		var curr = x[i];
+	var randomData = dataPrep(document.getElementById("dataInput").value); // Preparing the data
+	// insertion sort algorithm
+	for (var i = 1; i < randomData.length; i++) {
+		var curr = randomData[i];
 		var temp = i;
-		while (temp > 0 && x[temp - 1] >= curr) {
-			var tempVal = x[temp - 1];
-			x[temp] = tempVal;
+		while (temp > 0 && randomData[temp - 1] >= curr) {
+			var tempVal = randomData[temp - 1];
+			randomData[temp] = tempVal;
 			--temp;
 		}
-		x[temp] = curr;
+		randomData[temp] = curr;
 	}
-	document.getElementById("resultId").innerHTML = x;
+
+	 updateResult(randomData); // updating the result section with the sorted array
+}
+
+function updateResult(result){
+	document.getElementById("resultId").innerHTML = result;
 }
